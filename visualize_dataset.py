3c616f8a9b6dbd63619bad3e8fd9c6c9bf33a11c
frt03 <furuta0313@gmail.com>
2023-08-21 19:13:58 +0900

    fix visualize_dataset.py

--- visualize_dataset.py
+++ visualize_dataset.py
@@ -50,15 +50,16 @@ for i, episode in enumerate(ds.take(5)):
         plt.title(caption)
 
 # visualize action and state statistics
-actions, states = [], []
+# actions, states = [], []
+actions = [], []
 for episode in tqdm.tqdm(ds.take(500)):
     for step in episode['steps']:
         actions.append(step['action'].numpy())
-        states.append(step['observation']['state'].numpy())
+        # states.append(step['observation']['state'].numpy())
 actions = np.array(actions)
-states = np.array(states)
+# states = np.array(states)
 action_mean = actions.mean(0)
-state_mean = states.mean(0)
+# state_mean = states.mean(0)
 
 def vis_stats(vector, vector_mean, tag):
     assert len(vector.shape) == 2
@@ -76,7 +77,7 @@ def vis_stats(vector, vector_mean, tag):
         wandb.log({tag: wandb.Image(fig)})
 
 vis_stats(actions, action_mean, 'action_stats')
-vis_stats(states, state_mean, 'state_stats')
+# vis_stats(states, state_mean, 'state_stats')
 
 if not render_wandb:
     plt.show()
